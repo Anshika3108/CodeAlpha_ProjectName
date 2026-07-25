a5b469b363d3d182e24be447a2a55c21521944b1
ANSHIKA <anshika31082006@gmail.com>
2026-07-25 10:34:02 +0530

    Implement basic chatbot functionality

---
+++ chatbot.py
@@ -0,0 +1,20 @@
+def chatbot():
+    print("🤖 ChatBot: Hello! Type 'bye' to exit.")
+
+    while True:
+        user = input("You: ").lower()
+
+        if user == "hello":
+            print("🤖 ChatBot: Hi!")
+        elif user == "how are you":
+            print("🤖 ChatBot: I'm fine, thanks!")
+        elif user == "what is your name":
+            print("🤖 ChatBot: I'm a simple Python chatbot.")
+        elif user == "bye":
+            print("🤖 ChatBot: Goodbye!")
+            break
+        else:
+            print("🤖 ChatBot: Sorry, I don't understand that.")
+
+# Run the chatbot
+chatbot()
